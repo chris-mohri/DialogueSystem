bd4b28d807f55bb90512f56d043f1f08d76f238b
cim18 <cim18@pitt.edu>
2024-02-22 17:41:12 -0500

    tryna get commands parsable
    stable

--- Assets/Scripts/DialogueSystemScripts/CommandsController.cs
+++ Assets/Scripts/DialogueSystemScripts/CommandsController.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
 
 public class CommandsController : MonoBehaviour
 {
 
-    string baseImgDirectory = "Assets/Images/";
+    private string baseImgDirectory = "Assets/Images/";
 
     //data objects to hold important information/items
-    public Dictionary<string, int> routeInformation;
+    public Dictionary<string, int> routeInfo;
     public Dictionary<string, int> inventory;
+
+    //the dialogue system script (needed to access methods)
+    private DialogueSystem ds;
     
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(baseImgDirectory);
+        ds = GetComponent<DialogueSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void alisterInvoke(string line){
+
     }
 
     //choices = what is displayed on screen
